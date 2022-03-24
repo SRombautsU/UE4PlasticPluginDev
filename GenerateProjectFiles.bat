e10056a42887b0da8944773293798c77de23bbd0
Sébastien Rombauts <sebastien.rombauts@gmail.com>
2022-03-24 14:06:05 +0100

    Update scripts and files

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -5,10 +5,19 @@ set ROOTPATH=%~dp0
 
 pushd %ROOTPATH%
 
-set UEPATH=C:\Program Files\Epic Games\UE_4.27\
-set UBT="%UEPATH%Engine\Binaries\DotNET\UnrealBuildTool.exe"
-set PROJECT=UE4PlasticPluginDev
-set UPROJECT="%ROOTPATH%\%PROJECT%.uproject"
+set UBT="C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\DotNET\UnrealBuildTool.exe"
+if not exist %UBT% (
+    echo %UBT% not found
+    exit /b
+)
+
+set UPROJECT=
+for %%a in (*.uproject) do set "UPROJECT=%CD%\%%a"
+if not defined UPROJECT (
+    echo *.uproject file not found
+    exit /b
+)
+
 
 REM See Engine\Source\Programs\UnrealBuildTool\Modes\GenerateProjectFilesMode.cs
 REM extract:
--- build.bat
+++ build.bat
@@ -5,10 +5,21 @@ set ROOTPATH=%~dp0
 
 pushd %ROOTPATH%
 
-set UEPATH=C:\Program Files\Epic Games\UE_4.27\
-set UBT="%UEPATH%Engine\Binaries\DotNET\UnrealBuildTool.exe"
-set PROJECT=UE4PlasticPluginDev
-set UPROJECT="%ROOTPATH%\%PROJECT%.uproject"
+set UBT="C:\Program Files\Epic Games\UE_4.27\Engine\Binaries\DotNET\UnrealBuildTool.exe"
+if not exist %UBT% (
+    echo %UBT% not found
+    exit /b
+)
+
+for %%a in (*.uproject) do set "UPROJECT=%CD%\%%a"
+if not defined UPROJECT (
+    echo *.uproject file not found
+    exit /b
+)
+
+for %%i in ("%UPROJECT%") do (
+  set PROJECT=%%~ni
+)
 
 echo on
 %UBT% %UPROJECT% Win64 Development %PROJECT%Editor
