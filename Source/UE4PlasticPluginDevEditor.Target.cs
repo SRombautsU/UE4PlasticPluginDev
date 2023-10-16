7d8ecefc0e3d3cf1ae38af4efed5c4fed42fed0d
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2023-10-16 16:20:09 +0200

    bUseUnityBuild = false;

--- Source/UE4PlasticPluginDevEditor.Target.cs
+++ Source/UE4PlasticPluginDevEditor.Target.cs
@@ -11,5 +11,9 @@ public class UE4PlasticPluginDevEditorTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 
 		ExtraModuleNames.AddRange( new string[] { "UE4PlasticPluginDev" } );
+
+		// Uncomment to rebuild the whole project without Unity Build, compiling each cpp source file individually, in order to test Includ Whay You Use (IWYU) policy
+		// WARNING: don't uncomment on UnrealEngine Source Build, else you will trigger a full new Engine build (but easy to revert, will just relink 1200 lib/dll)
+		bUseUnityBuild = false;
 	}
 }
