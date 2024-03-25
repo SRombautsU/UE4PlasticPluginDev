29f875698b4a999725bf5774b8bd4e6d284a8ef0
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2024-03-25 18:28:08 +0100

    Enable WindowsPlatform.bStrictConformanceMode required to build starting from UE 5.4

--- Source/UE4PlasticPluginDevEditor.Target.cs
+++ Source/UE4PlasticPluginDevEditor.Target.cs
@@ -10,10 +10,14 @@ public class UE4PlasticPluginDevEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 
-		ExtraModuleNames.AddRange( new string[] { "UE4PlasticPluginDev" } );
+		// TODO Required for building with UE5.4
+//		DefaultBuildSettings = BuildSettingsVersion.V5;
+		WindowsPlatform.bStrictConformanceMode = true;
 
-		// Uncomment to rebuild the whole project without Unity Build, compiling each cpp source file individually, in order to test Includ Whay You Use (IWYU) policy
-		// WARNING: don't uncomment on UnrealEngine Source Build, else you will trigger a full new Engine build (but easy to revert, will just relink 1200 lib/dll)
-		bUseUnityBuild = false;
+		ExtraModuleNames.Add("UE4PlasticPluginDev");
+
+        // Uncomment to rebuild the whole project without Unity Build, compiling each cpp source file individually, in order to test Includ Whay You Use (IWYU) policy
+        // WARNING: don't uncomment on UnrealEngine Source Build, else you will trigger a full new Engine build (but easy to revert, will just relink 1200 lib/dll)
+        bUseUnityBuild = false;
 	}
 }
